95a85471a6193070c4f6df99362f52e653fc7301
Matheus-o-alves <matheus.oliveiraju@gmail.com>
2020-10-20 01:49:52 -0300

    firt commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './style/global.css';
+import './style/page/lading.css';
+import logoImg from './img/Logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='page-lading'>
+      <div className='content-wrapper'>
+        <img src={logoImg} alt='Happy'/>
+      </div>
+     
+     <main>
+       <h1>Leve Felicidade ao Mundo</h1>
+       <p>Visite orfanatos e mude o dia
+de muitas crianças.</p>
+     </main>
+     <div className='location'>
+       <strong>Ouro Preto -</strong>
+       <span> Minas Gerais</span>
+       </div>
+       <a href='' className='enter-app' ></a>
+
+     
     </div>
   );
 }
